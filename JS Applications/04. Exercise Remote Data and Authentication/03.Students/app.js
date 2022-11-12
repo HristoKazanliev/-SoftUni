6b8ea74bda5f3defed96ddbe7b6a7f73bdf31e75
Hristo Kazanliev <118056676+HristoKazanliev@users.noreply.github.com>
2022-11-12 18:52:55 +0200

    Solved 03. Students

--- JS Applications/04. Exercise Remote Data and Authentication/03.Students/app.js
+++ JS Applications/04. Exercise Remote Data and Authentication/03.Students/app.js
@@ -1,1 +1,44 @@
-console.log('TODO...');
+async function solve() {
+    const url = 'http://localhost:3030/jsonstore/collections/students';
+
+    const form = document.getElementById('form');
+    const tbody = document.querySelector('tbody');
+    form.addEventListener('submit', submitFunc);
+
+    tbody.innerHTML = '';
+    let response = await fetch(url);
+    let data = await response.json();
+    let allStudents = Object.values(data);
+
+    allStudents.forEach(student => {
+        let row = document.createElement('tr');
+
+        for (const item in student) {
+            if (item == '_id') continue
+
+            let td = document.createElement('td');
+            td.textContent = student[item];
+            row.appendChild(td);
+        }
+        tbody.appendChild(row);
+    });
+
+    async function submitFunc(e) {
+        e.preventDefault();
+
+        const data = new FormData(form);
+        const studentObj = Object.fromEntries(data.entries());
+
+        if (Object.values(studentObj).includes('')) {
+            return
+        }
+
+        await fetch(url, {
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(studentObj)
+        });
+    }
+}
+
+solve();
