edea94685a9b0de8db6c5f6348e84a5703c2e0a5
Hristo Kazanliev <118056676+HristoKazanliev@users.noreply.github.com>
2022-11-12 17:50:15 +0200

    Completed 02. Phonebook

--- JS Applications/04. Exercise Remote Data and Authentication/02.Phonebook/app.js
+++ JS Applications/04. Exercise Remote Data and Authentication/02.Phonebook/app.js
@@ -1,5 +1,51 @@
 function attachEvents() {
-    console.log('TODO...');
+    const url = 'http://localhost:3030/jsonstore/phonebook';
+
+    const phoneBook = document.getElementById('phonebook');
+    document.getElementById('btnLoad').addEventListener('click', loadFunc);
+    document.getElementById('btnCreate').addEventListener('click', createFunc);
+
+    async function loadFunc() {
+        phoneBook.innerHTML = '';
+
+        let response = await fetch(url);
+        let data = await response.json();
+
+        for (const item of Object.values(data)) {
+            let deleteBtn = document.createElement('button');
+            deleteBtn.textContent = 'Delete';
+            deleteBtn.addEventListener('click', deleteFunc);
+
+            let liElement = document.createElement('li');
+            liElement.id = item._id;
+            liElement.textContent = `${item.person}: ${item.phone}`;
+            liElement.appendChild(deleteBtn);
+            phoneBook.appendChild(liElement);
+        }
+    };
+
+    async function createFunc() {
+        let person = document.getElementById('person').value;
+        let phone = document.getElementById('phone').value;
+        
+        await fetch(url, {
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({person, phone})
+        });
+
+        person.value = '';
+        phone.value = '';
+        loadFunc();
+    };
+
+    async function deleteFunc(e) {
+        let id = e.target.parentElement.id;
+
+        await fetch(`${url}/${id}`, {
+            method: 'delete'
+        });
+    };
 }
 
 attachEvents();
