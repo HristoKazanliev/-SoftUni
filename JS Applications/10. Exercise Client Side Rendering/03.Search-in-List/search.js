d6c2f81fb18494bfbc80f8243eee672609a3d991
Hristo Kazanliev <118056676+HristoKazanliev@users.noreply.github.com>
2022-11-27 13:37:17 +0200

    Solved 03. Search in List

--- JS Applications/10. Exercise Client Side Rendering/03.Search-in-List/search.js
+++ JS Applications/10. Exercise Client Side Rendering/03.Search-in-List/search.js
@@ -1,3 +1,30 @@
+import { html, render } from './node_modules/lit-html/lit-html.js';
+import { towns } from './towns.js';
+
+const root = document.getElementById('towns');
+const result = document.getElementById('result');
+
+const townsTemplate = html`
+<ul>
+   ${towns.map(town => html`<li>${town}</li>`)}
+</ul>`;
+
+render(townsTemplate, root);
+
+document.querySelector('button').addEventListener('click', search);
 function search() {
-   // TODO
+   const searchText = document.getElementById('searchText').value;
+   const townList = root.querySelectorAll('li');
+   let count = 0;
+
+   townList.forEach(town => {
+      if (town.textContent.includes(searchText)) {
+         town.classList.add('active');
+         count++;
+      }else{
+         town.classList.remove('active');
+      }
+   });
+
+   result.textContent = `${count} matches found`;
 }
