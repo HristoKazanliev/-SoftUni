3b877eab957c21a26812360ec80413c300cf8366
Hristo Kazanliev <118056676+HristoKazanliev@users.noreply.github.com>
2023-02-03 22:51:56 +0200

    Completed 10-17

--- MS SQL/04. Built-in Functions/Built-in-Functions.sql
+++ MS SQL/04. Built-in Functions/Built-in-Functions.sql
@@ -64,3 +64,74 @@ SELECT * FROM V_EmployeesHiredAfter2000
 SELECT FirstName, LastName 
 FROM Employees
 WHERE LEN(LastName) = 5
+
+--10 Rank Employees by Salary
+SELECT EmployeeID, FirstName, LastName, Salary,
+	DENSE_RANK() OVER
+	(PARTITION BY Salary ORDER BY EmployeeID) AS Rank
+FROM Employees
+WHERE Salary BETWEEN 10000 AND 50000
+ORDER BY Salary DESC
+
+--11 Find All Employees with Rank 2
+SELECT * FROM 
+(
+	SELECT EmployeeID, FirstName, LastName, Salary,
+		DENSE_RANK() OVER
+		(PARTITION BY Salary ORDER BY EmployeeID) AS Rank
+	FROM Employees
+	WHERE Salary BETWEEN 10000 AND 50000
+)	AS RankingSubquery
+WHERE Rank = 2
+ORDER BY Salary DESC
+
+--12 Countries Holding 'A' 3 or More Times
+USE Geography
+SELECT CountryName, IsoCode 
+FROM Countries
+WHERE CountryName LIKE '%A%A%A%'
+ORDER BY IsoCode
+
+--13 Mix of Peak and River Names
+SELECT p.PeakName,
+		r.RiverName,
+		LOWER(CONCAT(LEFT(p.PeakName, LEN(p.PeakName) - 1), r.RiverName)) AS Mix
+FROM Peaks AS p, Rivers AS r
+WHERE RIGHT(p.PeakName, 1) = LEFT(r.RiverName, 1)
+ORDER BY Mix
+
+USE Diablo
+--14 Games From 2011 and 2012 Year
+SELECT TOP(50) Name, FORMAT([Start], 'yyyy-MM-dd') AS [Start]
+FROM GAMES
+WHERE YEAR([Start]) IN (2011, 2012) 
+ORDER BY Start, Name
+
+--15 User Email Providers
+SELECT Username,
+		SUBSTRING(Email, CHARINDEX('@', Email) + 1, LEN(Email)) AS [Email Provider]
+FROM Users
+ORDER BY [Email Provider], Username
+
+--16 Get Users with IP Address Like Pattern
+SELECT Username, IpAddress
+FROM Users
+WHERE IpAddress LIKE '___.1%.%.___'
+ORDER BY Username
+
+
+--17 Show All Games with Duration & Part of the Day
+SELECT [Name] AS Game,
+		CASE
+			WHEN DATEPART(HOUR, Start) BETWEEN 0 AND 11 THEN 'Morning'
+			WHEN DATEPART(HOUR, Start) BETWEEN 12 AND 17 THEN 'Afternoon'
+			ELSE 'Evening'
+		END AS [Part of the Day],
+		CASE
+			WHEN Duration BETWEEN 0 AND 3 THEN 'Extra Short'
+			WHEN Duration BETWEEN 4 AND 6 THEN 'Short'
+			WHEN Duration > 6  THEN 'Long'
+			ELSE 'Extra Long'
+		END AS Duration
+FROM Games
+ORDER BY [Name], [Duration], [Part of the Day]
