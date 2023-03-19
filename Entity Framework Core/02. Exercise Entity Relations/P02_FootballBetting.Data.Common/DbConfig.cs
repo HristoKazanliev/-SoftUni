0d802e1601372c03f5c2317008771377255281b6
Hristo Kazanliev <118056676+HristoKazanliev@users.noreply.github.com>
2023-03-19 16:42:44 +0200

    Solved 02. Football Betting

---
+++ Entity Framework Core/02. Exercise Entity Relations/P02_FootballBetting.Data.Common/DbConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace P02_FootballBetting.Data.Common
+{
+    public class DbConfig
+    {
+        public const string ConnectionString =
+            @"Server=DESKTOP-KE01FV1\SQLEXPRESS01;Database=BET365;Integrated Security=True;Encrypt=False;";
+    }
+}
---
+++ Entity Framework Core/02. Exercise Entity Relations/P02_FootballBetting.Data/Migrations/20230319142612_PlayerIdFixed.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace P02_FootballBetting.Data.Migrations
+{
+    public partial class PlayerIdFixed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PlayerIdId",
+                table: "Players",
+                newName: "PlayerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PlayerId",
+                table: "Players",
+                newName: "PlayerIdId");
+        }
+    }
+}
