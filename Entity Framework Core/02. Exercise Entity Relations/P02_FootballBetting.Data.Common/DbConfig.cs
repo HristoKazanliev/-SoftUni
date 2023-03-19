dee80849fc4595aa59f3ec45ea56526e8d73917a
Hristo Kazanliev <118056676+HristoKazanliev@users.noreply.github.com>
2023-03-19 16:51:22 +0200

    Update DbConfig

--- Entity Framework Core/02. Exercise Entity Relations/P02_FootballBetting.Data.Common/DbConfig.cs
+++ Entity Framework Core/02. Exercise Entity Relations/P02_FootballBetting.Data.Common/DbConfig.cs
@@ -3,6 +3,6 @@
     public class DbConfig
     {
         public const string ConnectionString =
-            @"Server=DESKTOP-KE01FV1\SQLEXPRESS01;Database=BET365;Integrated Security=True;Encrypt=False;";
+            @"Server=.;Database=BET365;Integrated Security=True;Encrypt=False;";
     }
 }
