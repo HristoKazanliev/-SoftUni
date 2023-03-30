43d9e68c3e9d8a8c6acd3f342c388a3ea9e7ddc6
Hristo Kazanliev <118056676+HristoKazanliev@users.noreply.github.com>
2023-03-30 23:54:16 +0300

    Solved 9, 10 from Car Dealer

---
+++ Entity Framework Core/06. Exercise XML Processing/CarDealer/StartUp.cs
@@ -0,0 +1,87 @@
+﻿using AutoMapper;
+using CarDealer.Data;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+using System.IO;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            CarDealerContext context= new CarDealerContext();
+
+            //string inputXml = File.ReadAllText(@"../../../Datasets/suppliers.xml");
+            //Console.WriteLine(ImportSuppliers(context,inputXml));
+
+            string inputXml = File.ReadAllText(@"../../../Datasets/parts.xml");
+            Console.WriteLine(ImportParts(context, inputXml));
+        }
+
+        //09
+        public static string ImportSuppliers(CarDealerContext context, string inputXml)
+        {
+            IMapper mapper = InitializeAutoMapper();
+            XmlHelper xmlHelper = new XmlHelper();
+
+            ImportSupplierDto[] supplierDtos = xmlHelper.Deserialize<ImportSupplierDto[]>(inputXml, "Suppliers");
+
+            ICollection<Supplier> validSuppliers = new HashSet<Supplier>();
+
+            foreach (var supplierDto in supplierDtos)
+            {
+                if (string.IsNullOrEmpty(supplierDto.Name))
+                {
+                    continue;
+                }
+
+                Supplier supplier = mapper.Map<Supplier>(supplierDto);
+
+                validSuppliers.Add(supplier);
+            }
+
+            context.Suppliers.AddRange(validSuppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {validSuppliers.Count}";
+        }
+
+        public static string ImportParts(CarDealerContext context, string inputXml)
+        {
+            IMapper mapper = InitializeAutoMapper();
+            XmlHelper xmlHelper = new XmlHelper();
+
+            ImportPartDto[] partDtos = xmlHelper.Deserialize<ImportPartDto[]>(inputXml, "Parts");
+
+            ICollection<Part> validParts = new HashSet<Part>();
+
+            foreach (var partDto in partDtos)
+            {
+                if (string.IsNullOrEmpty(partDto.Name))
+                {
+                    continue;
+                }
+
+                if (!partDto.SupplierId.HasValue || !context.Suppliers.Any(s => s.Id == partDto.SupplierId))
+                {
+                    continue;
+                }
+
+                Part part= mapper.Map<Part>(partDto);
+                validParts.Add(part);
+            }
+
+            context.Parts.AddRange(validParts);
+            context.SaveChanges();
+
+            return $"Successfully imported {validParts.Count}";
+        }
+
+        private static IMapper InitializeAutoMapper()
+            => new Mapper(new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<CarDealerProfile>();
+            }));
+    }
+}
---
+++ Entity Framework Core/06. Exercise XML Processing/CarDealer/XmlHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace CarDealer
+{
+    public class XmlHelper
+    {
+        public T Deserialize<T>(string inputXml, string rootName)
+        {
+            XmlRootAttribute xmlRoot = new XmlRootAttribute(rootName);
+            XmlSerializer xmlSerializer= new XmlSerializer(typeof(T), xmlRoot);
+
+            using StringReader xmlString = new StringReader(inputXml);
+            T deserializedDtos = (T)xmlSerializer.Deserialize(xmlString);
+
+            return deserializedDtos;
+        }
+
+        public IEnumerable<T> DeserializeCollection<T>(string inputXml, string rootName)
+        {
+            XmlRootAttribute xmlRoot = new XmlRootAttribute(rootName);
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T[]), xmlRoot);
+
+            using StringReader xmlString = new StringReader(inputXml);
+            T[] deserializedDtos = (T[])xmlSerializer.Deserialize(xmlString);
+
+            return deserializedDtos;
+        }
+    }
+}
