8bc28d1c88e6e288cdafe2fe8f493551e52bafb0
Hristo Kazanliev <118056676+HristoKazanliev@users.noreply.github.com>
2023-04-01 00:43:51 +0300

    Exam - 15 August 2022

---
+++ Entity Framework Core/Exam - 15 August 2022/Trucks/DataProcessor/Serializer.cs
@@ -0,0 +1,72 @@
+﻿namespace Trucks.DataProcessor
+{
+    using CarDealer;
+    using Data;
+    using Microsoft.EntityFrameworkCore;
+    using Newtonsoft.Json;
+    using Trucks.Data.Models;
+    using Trucks.DataProcessor.ExportDto;
+
+    public class Serializer
+    {
+        public static string ExportDespatchersWithTheirTrucks(TrucksContext context)
+        {
+            XmlHelper xmlHelper = new XmlHelper();
+
+            ExportDespatcherDto[] despatcherDtos = context.Despatchers
+                .Where(d => d.Trucks.Any())
+                .Select(d => new ExportDespatcherDto()
+                {
+                    DespatcherName = d.Name,
+                    TrucksCount = d.Trucks.Count,
+                    Trucks = d.Trucks
+                            .Select(t => new ExportTruckDto()
+                            {
+                                RegistrationNumber = t.RegistrationNumber,
+                                Make = t.MakeType.ToString()
+                            })
+                            .OrderBy(t => t.RegistrationNumber)
+                            .ToArray()
+                })
+                .OrderByDescending(d => d.TrucksCount)
+                .ThenBy(d => d.DespatcherName)
+                .ToArray();
+
+            return xmlHelper.Serialize(despatcherDtos, "Despatchers");
+        }
+
+        public static string ExportClientsWithMostTrucks(TrucksContext context, int capacity)
+        {
+            var clients = context.Clients
+                .Include(c => c.ClientsTrucks)
+                .ThenInclude(ct => ct.Truck)
+                .AsNoTracking()
+                .ToArray()
+                .Where(c => c.ClientsTrucks.Any(ct => ct.Truck.TankCapacity >= capacity))
+                .Select(c => new
+                {
+                    c.Name,
+                    Trucks = c.ClientsTrucks
+                        .Where(ct => ct.Truck.TankCapacity >= capacity)
+                        .Select(ct => new
+                        {
+                            TruckRegistrationNumber = ct.Truck.RegistrationNumber,
+                            VinNumber = ct.Truck.VinNumber,
+                            TankCapacity = ct.Truck.TankCapacity,
+                            CargoCapacity = ct.Truck.CargoCapacity,
+                            CategoryType = ct.Truck.CategoryType.ToString(),
+                            MakeType = ct.Truck.MakeType.ToString()
+                        })
+                        .OrderBy(t => t.MakeType)
+                        .ThenByDescending(t => t.CargoCapacity)
+                        .ToArray()
+                })
+                .OrderByDescending(c => c.Trucks.Length)
+                .ThenBy(c => c.Name)
+                .Take(10)
+                .ToArray();
+
+            return JsonConvert.SerializeObject(clients, Formatting.Indented);
+        }
+    }
+}
