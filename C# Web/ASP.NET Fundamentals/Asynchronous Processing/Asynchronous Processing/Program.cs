35a0ad042bafa1d4293fcf5a929fb5b6b9354491
Hristo Kazanliev <118056676+HristoKazanliev@users.noreply.github.com>
2023-06-20 18:00:46 +0300

    Added Asynchronous Processing Lab

---
+++ C# Web/ASP.NET Fundamentals/Asynchronous Processing/Asynchronous Processing/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Asynchronous_Processing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+
+            Thread thread = new Thread(() => PrintEvenNumbers(start,end));
+            thread.Start();
+            thread.Join();
+            Console.WriteLine("Thread finished work ");
+        }
+
+        private static void PrintEvenNumbers(int start, int end)
+        {
+            for (int i = start; i <= end; i++) 
+            {
+                if (i % 2 == 0) Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ C# Web/ASP.NET Fundamentals/Asynchronous Processing/SumEvensInRange/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace SumEvensInRange
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) 
+            {
+                var cmd = Console.ReadLine();
+                if (cmd == "show") 
+                {
+                    var result = SumAsync();
+                    Console.WriteLine(result);
+                }
+            }
+        }
+
+        private static long SumAsync()
+        {
+            return Task.Run(() =>
+            {
+                long sum = 0;
+                for (int i = 0; i < 1000; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        Console.WriteLine(sum += i);
+                    }
+                }
+
+                return sum;
+            }).Result;
+        }
+    }
+}
