9c79f54fb669c889aa5bab1bd7d141ac8895a4a1
Hristo Kazanliev <118056676+HristoKazanliev@users.noreply.github.com>
2023-10-21 19:02:12 +0300

    Data Structures Fundamentals (with C#) Repository

---
+++ C# Data Structures Fundamentals/01.Linear Data Structures/TestSolutions/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace TestSolutions
+{
+    using Problem01.List;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int>();
+            list.Add(1);
+            foreach (int i in list) 
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
